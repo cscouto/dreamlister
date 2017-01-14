6d38e390fabc362f66f1c1a2b2af1407b8e5ef17
Tiago Henrique <tiagocouto@Tiagos-iMac.local>
2017-01-14 11:31:15 -0800

    entities

---
+++ DreamLister/Image+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Tiago Henrique on 1/14/17.
+//  Copyright © 2017 Tiago Henrique. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
---
+++ DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Tiago Henrique on 1/14/17.
+//  Copyright © 2017 Tiago Henrique. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
