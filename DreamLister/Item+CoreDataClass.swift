0075de646409093074a81bcf3fd973120ae141b9
Tiago Henrique <tiagocouto@Tiagos-iMac.local>
2017-01-17 16:42:08 -0800

    ui

--- DreamLister/Item+CoreDataClass.swift
+++ DreamLister/Item+CoreDataClass.swift
@@ -11,6 +11,12 @@ import Foundation
 import CoreData
 
 @objc(Item)
-public class Item: NSManagedObject {
 
+public class Item: NSManagedObject {
+    
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+
+        self.created = NSDate()
+    }
 }
