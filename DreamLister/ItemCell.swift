b102a92348c163a9b040e35c88cfc589b09a7f78
Tiago Henrique <tiagocouto@Tiagos-iMac.local>
2017-01-17 16:56:37 -0800

    tableview

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Tiago Henrique on 1/17/17.
+//  Copyright © 2017 Tiago Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet var thumb: UIImageView!
+    @IBOutlet var itemName: UILabel!
+    @IBOutlet var itemPrice: UILabel!
+    @IBOutlet var details: UILabel!
+    
+
+}
--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
 }
 
